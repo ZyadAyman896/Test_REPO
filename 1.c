f3f072abc433f668b814e927f4ed8c67d531ef63
Zyad <[zyadayman254@gmail.com]>
2024-07-29 14:27:48 +0300

    adding this new function

--- 1.c
+++ 1.c
@@ -1,11 +1,18 @@
 
 #include <stdio.h>
-
+#include "1.h"
 
 int main()
 {
 
 	printf("hello from z");
-
+	printf("%i" ,  Add(5, 5));
 	return 0 ;
+}
+
+int Add(int x , int y)
+{
+
+	return x + y ;
+
 }
